7bbd3fe216ae6020be1f289285bb173c1ba871ab
Rafiulsam <rafiulsam69@gmail.com>
2023-08-31 15:44:54 +0600

    calculate grand total

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -1,18 +1,23 @@
 import React from 'react';
 import './Cart.css'
 const Cart = ({ cart }) => {
-    let total = 0
+    let totalPrice = 0
+    let totalShipping = 0
+
     for (const product of cart) {
-        total += product.price
+        totalPrice += product.price
+        totalShipping += product.shipping
     }
+    const tax = totalPrice*5/100
+    const grandTotal = totalPrice + totalShipping + tax
     return (
         <div className='cart'>
-            <h3 style={{ textAlign: 'center', fontWeight:"400", fontSize:'25px'}}>Oder summary</h3>
+            <h3 style={{ textAlign: 'center', fontWeight: "400", fontSize: '25px' }}>Oder summary</h3>
             <p>Selected Items: {cart.length}</p>
-            <p>Total Price: ${total}</p>
-            <p>Shipping Charge: $</p>
-            <p>Tax: $</p>
-            <h4 style={{fontSize:"21px", fontWeight:"400"}}>Grand Total:</h4>
+            <p>Total Price: ${totalPrice}</p>
+            <p>Shipping Charge: ${totalShipping}</p>
+            <p>Tax: ${tax}</p>
+            <h4 style={{ fontSize: "21px", fontWeight: "400" }}>Grand Total: ${grandTotal}</h4>
         </div>
     );
 };
